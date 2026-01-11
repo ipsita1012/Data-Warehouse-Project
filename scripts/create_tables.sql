f856cc153a503673ae62ea3a43fbe7e491fe6ddd
Ipsita Sahu <ipsitasahu51@gmail.com>
2026-01-11 14:20:55 -0800

    Update and rename create_tables to create_tables.sql

--- scripts/create_tables.sql
+++ scripts/create_tables.sql
@@ -1,3 +1,12 @@
+/* 
+==========================================================
+DDL Script:: Create Data Tables for Bronze Layer
+==========================================================
+Script Purpose::
+	This script creates tables in the 'bronze' schema, droppling the existing table if already exists.
+	Run this script to re-define the DDL structure of 'bronze' tables.
+==========================================================
+
 IF OBJECT_ID ('bronze.crm_cust_info', 'U') IS NOT NULL
 	DROP TABLE bronze.crm_cust_info;
 
